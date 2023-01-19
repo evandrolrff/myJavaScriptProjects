f4607c1bd36eea44170cc079faa30319e3f8d1b2
Evandro <evandrolrff@gmail.com>
2023-01-18 21:39:26 -0300

    dark mode

---
+++ Dark-mode/data.js
@@ -0,0 +1,12 @@
+const articles = [
+    {
+        id: 1,
+        title: 'Test Title',
+        date: new Date(2020, 9, 4),
+        length: 11,
+        snippet: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto libero, saepe voluptatibus ratione rem amet dolor fugit quod obcaecati tempore nesciunt laborum explicabo culpa repudiandae laudantium, ipsum labore voluptates.
+        Assumenda fuga deserunt quae in corrupti, quaerat porro perferendis suscipit inventore veritatis commodi minima? Numquam esse sed fuga corporis recusandae debitis voluptatibus! Saepe dolorem sint accusamus nulla quisquam culpa voluptates.
+        Iure nisi reprehenderit mollitia itaque velit molestiae? Quasi impedit praesentium facilis excepturi dolorum hic? Fuga eius quos quae praesentium unde quia ratione, nulla modi, iusto assumenda corrupti officia doloremque iure!
+        Consequuntur, accusamus maiores, aliquid ipsum fuga autem tempore non magni tenetur quos, error officiis esse. Et voluptatum modi nisi ratione magnam quasi, perferendis, explicabo dolore assumenda optio vitae, repudiandae voluptatem?
+        Recusandae quidem dolorum dignissimos ducimus reiciendis. Laboriosam veniam deserunt odit eos at perspiciatis cum, id totam delectus! Quo at mollitia animi voluptas laborum optio impedit eius doloremque aliquid. Alias, reiciendis!`,
+    }]
